4d4049aa37299af30b3c78914aa1484fc3ae3586
starpro123 <starpro123@naver.com>
2022-04-02 00:56:18 +0900

    챕터4: 데이터 중심 설계 시작

---
+++ alibreo/DataOrientedMovie/src/main/java/Movie.java
@@ -0,0 +1,10 @@
+public class Movie {
+    private String title;
+    private Duration runningTIme;
+    private Money fee;
+    private List<DiscountCondition> discountConditionList;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+}
